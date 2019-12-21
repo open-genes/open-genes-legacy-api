71ca5b98cb8a936a8514e1fbcc81540e56de1aa7
bdf2ch <bdf2ch@gmail.com>
2019-12-21 16:36:33 +0300

    API url fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://dev.open-genes.com',
+  apiUrl: 'http://open-genes.com',
   pubmedApiUrl: 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils',
   pubmedUrl: 'https://www.ncbi.nlm.nih.gov/pubmed/',
   languages: ['ru', 'en']
