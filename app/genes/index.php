5c753fe3e64fc481243db1b136a070e5d4c34cb8
N.Minyaev <miniaevnikita@yandex.ru>
2021-01-12 19:38:38 +0400

    #OLD-143 Убрать некоторые зависимости и удалить неиспользуемые

--- app/genes/index.php
+++ app/genes/index.php
@@ -8,7 +8,6 @@ defined('YII_ENV') or define('YII_ENV', getenv('ENV'));
 
 require __DIR__ . '/../common/vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../common/config/bootstrap.php';
-require __DIR__ . '/config/bootstrap.php';
 
 $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../common/config/main.php',
