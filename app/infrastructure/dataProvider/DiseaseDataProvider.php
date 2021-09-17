66772b126340981c76d559abf9548a70d286833b
imhelle <sp.olga.inf@gmail.com>
2021-09-17 21:31:40 +0300

    OG-176 add diseases to icd categories

--- app/infrastructure/dataProvider/DiseaseDataProvider.php
+++ app/infrastructure/dataProvider/DiseaseDataProvider.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\infrastructure\dataProvider;
 
 
@@ -6,7 +7,7 @@ use app\models\Disease;
 
 class DiseaseDataProvider implements DiseaseDataProviderInterface
 {
-
+    
     public function getAllDiseases($lang): array
     {
         $nameField = $lang == 'en-US' ? 'name_en' : 'name_ru';
@@ -21,8 +22,8 @@ class DiseaseDataProvider implements DiseaseDataProviderInterface
     public function getDiseasesCategories($lang): array
     {
         $icdNameField = $lang == 'en-US' ? 'icd_name_en' : 'icd_name_ru';
-        return Disease::find()
-            ->select(['disease_category.icd_code', 'disease_category.' . $icdNameField . ' icd_category_name'])
+        $rootCategories = Disease::find()
+            ->select(['disease_category.icd_code icdCode', 'disease_category.' . $icdNameField . ' icdCategoryName'])
             ->distinct()
             ->innerJoin('gene_to_disease', 'gene_to_disease.disease_id=disease.id')
             ->join(
@@ -33,5 +34,32 @@ class DiseaseDataProvider implements DiseaseDataProviderInterface
             ->where('disease_category.icd_name_en is not null')
             ->asArray()
             ->all();
+
+        foreach ($rootCategories as &$rootCategory) {
+            $rootCategory['diseases'] = $this->getChildrenDiseasesForIcdRecursive($rootCategory['icdCode'], $lang);
+        }
+        return $rootCategories;
+    }
+
+    private function getChildrenDiseasesForIcdRecursive($rootIcdCategory, $lang): array
+    {
+        $nameField = $lang == 'en-US' ? 'name_en' : 'name_ru';
+        $icdNameField = $lang == 'en-US' ? 'icd_name_en' : 'icd_name_ru';
+        $diseases = [];
+        $icdChildrenCategories = Disease::find()
+            ->select(['disease.id', 'disease.icd_code icdCode', $nameField . ' name', 
+                $icdNameField . ' icdName', 'group_concat(distinct gene.symbol separator ",") genesSymbols'])
+            ->innerJoin('gene_to_disease', 'gene_to_disease.disease_id=disease.id')
+            ->innerJoin('gene', 'gene_to_disease.gene_id=gene.id')
+            ->where(['icd_code_visible' => $rootIcdCategory])
+            ->groupBy('disease.id')
+            ->asArray()->all();
+        foreach ($icdChildrenCategories as $category) {
+            $categoryId = $category['id'];
+            unset($category['id']);
+            $category['genesSymbols'] = explode(',', $category['genesSymbols']);
+            $diseases[$categoryId] = $category;
+        }
+        return $diseases;
     }
 }
